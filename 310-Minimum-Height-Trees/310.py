66e0b6c2a818e515c5675b8de777f38293fe66d6
jeremyimmanuel <jeremy8@uw.edu>
2020-03-14 23:46:40 -0700

    first commit

---
+++ 310-Minimum-Height-Trees/310.py
@@ -0,0 +1,17 @@
+from collections import defaultdict as dd 
+
+def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
+    # base case
+    if n == 1:
+        return [0]
+    
+    
+
+
+
+def main():
+    print('hello world')
+
+
+if __name__ == '__main__':
+    main()
---
+++ README.md
@@ -0,0 +1,8 @@
+# Leetcode Solutions
+
+This is my personal repo to store leetcode solutions
+
+Some are my own solutions, some are not.
+I try to cite them if I get it from someone else.
+
+Hope this can help other people
